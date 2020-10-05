18ead5d31b243774f806ca93ed2af2022feb092f
valtermartins666 <58224064+valtermartins666@users.noreply.github.com>
2020-10-04 23:40:43 -0300

    Add files via upload

---
+++ TesteEntrevista/DadosCalculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEntrevista
+{
+    public class DadosCalculo : DadosLiberacao
+    {
+        public string QtdParcelas { get; set; }
+        public string Valor { get; set; }
+        public int TipoCredito { get; set; }
+        public string DataPrimeiroVencimento { get; set; }
+
+
+        
+        public StatusLiberacao StatusLiberacao { get; set; }
+    }
+}
---
+++ TesteEntrevista/DadosLiberacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEntrevista
+{
+    public class DadosLiberacao 
+    {
+        public decimal ValorTotalComJuros { get; set; }
+        public decimal ValorDoJuros { get; set; }
+        public bool LiberaValor { get; set; }
+        public bool LiberaParcela { get; set; }
+
+        public enum StatusLiberacao
+        {            
+            Reprovado=0,
+            Aprovado
+        }
+
+       
+    }
+}
